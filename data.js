7596f40ad98f3837b355ddf6ef627ef3a93a703e
amin515 <nil.somudra51@gmail.com>
2021-12-24 03:24:21 +0600

    addding new files

---
+++ data.js
@@ -0,0 +1,284 @@
+const devs = [{
+        id: 1,
+        name: 'Nur Amin',
+        age: 33,
+        loc: 'Bogura',
+        gender: 'Male',
+        photo: 'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+
+    },
+    {
+        id: 2,
+        name: 'Achem',
+        age: 16,
+        loc: 'Barisal',
+        gender: 'Male',
+        photo: 'https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+
+    },
+    {
+        id: 3,
+        name: 'Abu Sayeed',
+        age: 80,
+        loc: 'Cumilla',
+        gender: 'Male',
+        photo: 'https://image.shutterstock.com/image-photo/portrait-positive-cheerful-man-show-260nw-1531460657.jpg'
+
+    },
+    {
+        id: 4,
+        name: 'Sumaya Tasnim',
+        age: 25,
+        loc: 'Chuadanga',
+        gender: 'Female',
+        photo: 'https://image.shutterstock.com/image-photo/portrait-happy-mid-adult-man-260nw-1812937819.jpg'
+
+    },
+    {
+        id: 5,
+        name: 'Mamun Islam',
+        age: 37,
+        loc: 'Rangpur',
+        gender: 'Male',
+        photo: 'https://img.freepik.com/free-photo/young-handsome-man-with-beard-isolated-keeping-arms-crossed-frontal-position_1368-132662.jpg?size=626&ext=jpg'
+
+    },
+    {
+        id: 6,
+        name: 'Horipad bandwala',
+        age: 19,
+        loc: 'Kolkata',
+        gender: 'Male',
+        photo: 'https://www.morganstanley.com/content/dam/msdotcom/people/Careers/wide-audrey-gibson.jpg/_jcr_content/renditions/wide_16x9.jpg'
+
+    },
+    {
+        id: 7,
+        name: 'Barack Obama',
+        age: 60,
+        loc: 'Washington DC',
+        gender: 'Male',
+        photo: 'https://image.shutterstock.com/image-photo/portrait-men-studio-gray-background-260nw-328098230.jpg'
+
+    },
+    {
+        id: 8,
+        name: 'Boris Jhonson',
+        age: 68,
+        loc: 'London',
+        gender: 'Male',
+        photo: 'https://thumbs.dreamstime.com/b/portrait-handsome-smiling-young-man-folded-arms-isolated-gray-background-joyful-cheerful-men-crossed-hands-studio-shot-172868988.jpg'
+
+    },
+    {
+        id: 9,
+        name: 'Robiul Islam',
+        age: 38,
+        loc: 'Rangamati',
+        gender: 'Male',
+        photo: 'https://pagesix.com/wp-content/uploads/sites/3/2021/11/chris-evans069_.jpg?quality=80&strip=all&w=1024'
+
+    },
+    {
+        id: 10,
+        name: 'Humayn ahmed',
+        age: 26,
+        loc: 'Dhaka',
+        gender: 'Male',
+        photo: 'https://mymodernmet.com/wp/wp-content/uploads/2019/09/100k-ai-faces-8.jpg'
+
+    },
+    {
+        id: 11,
+        name: 'Marjuk russel',
+        age: 26,
+        loc: 'Uttara',
+        gender: 'Male',
+        photo: 'https://i.pinimg.com/originals/25/41/6b/25416bef3ed941310a877484cea1a20b.png'
+
+    },
+    {
+        id: 12,
+        name: 'Riyadh',
+        age: 26,
+        loc: 'Banani',
+        gender: 'Male',
+        photo: 'https://i.pinimg.com/originals/a6/82/f0/a682f04aaa23b09cf5c3ce162fd7fc79.jpg'
+
+    },
+];
+
+
+
+
+//==================student data==================
+
+const stu = [{
+        Roll: 12,
+        Name: 'Nur Amin',
+        Loc: 'Bogura',
+        Gender: 'Male',
+        photo: 'https://i.pinimg.com/originals/a6/82/f0/a682f04aaa23b09cf5c3ce162fd7fc79.jpg',
+        Bn: 33,
+        En: 40,
+        Math: 45,
+        S: 85,
+        SS: 78,
+        Rel: 85,
+    },
+    {
+        Roll: 87,
+        Name: 'Priyaka das',
+        Loc: 'Magura',
+        Gender: 'Female',
+        photo: 'https://i.pinimg.com/originals/25/41/6b/25416bef3ed941310a877484cea1a20b.png',
+        Bn: 75,
+        En: 74,
+        Math: 87,
+        S: 52,
+        SS: 40,
+        Rel: 30,
+    },
+    {
+        Roll: 32,
+        Name: 'Uttam Kimar',
+        Loc: 'Natore',
+        Gender: 'Male',
+        photo: 'https://i.pinimg.com/originals/a6/82/f0/a682f04aaa23b09cf5c3ce162fd7fc79.jpg',
+        Bn: 85,
+        En: 49,
+        Math: 76,
+        S: 75,
+        SS: 94,
+        Rel: 65,
+    },
+    {
+        Roll: 98,
+        Name: 'Mamun Islam',
+        Loc: 'Rangpur',
+        Gender: 'Male',
+        photo: 'https://i.pinimg.com/originals/a6/82/f0/a682f04aaa23b09cf5c3ce162fd7fc79.jpg',
+        Bn: 65,
+        En: 85,
+        Math: 76,
+        S: 78,
+        SS: 52,
+        Rel: 75,
+    },
+    {
+        Roll: 65,
+        Name: 'Abu sayeed',
+        Loc: 'Cumilla',
+        Gender: 'Male',
+        photo: 'https://i.pinimg.com/originals/a6/82/f0/a682f04aaa23b09cf5c3ce162fd7fc79.jpg',
+        Bn: 95,
+        En: 62,
+        Math: 89,
+        S: 63,
+        SS: 75,
+        Rel: 61,
+    },
+    {
+        Roll: 121,
+        Name: 'Yameen Hossain',
+        Loc: 'Bogura',
+        Gender: 'Male',
+        photo: 'https://i.pinimg.com/originals/a6/82/f0/a682f04aaa23b09cf5c3ce162fd7fc79.jpg',
+        Bn: 85,
+        En: 65,
+        Math: 41,
+        S: 45,
+        SS: 52,
+        Rel: 40,
+    },
+    {
+        Roll: 85,
+        Name: 'Soniya Bariki',
+        Loc: 'Netrokona',
+        Gender: 'Female',
+        photo: 'https://i.pinimg.com/originals/a6/82/f0/a682f04aaa23b09cf5c3ce162fd7fc79.jpg',
+        Bn: 39,
+        En: 50,
+        Math: 78,
+        S: 49,
+        SS: 40,
+        Rel: 45,
+    },
+    {
+        Roll: 22,
+        Name: 'Ratna Akter',
+        Loc: 'Dhunot',
+        Gender: 'Female',
+        photo: 'https://i.pinimg.com/originals/a6/82/f0/a682f04aaa23b09cf5c3ce162fd7fc79.jpg',
+        Bn: 51,
+        En: 45,
+        Math: 85,
+        S: 74,
+        SS: 41,
+        Rel: 42,
+    },
+    {
+        Roll: 25,
+        Name: 'Sayeem',
+        Loc: 'Kuri-Gram',
+        Gender: 'Male',
+        photo: 'https://i.pinimg.com/originals/a6/82/f0/a682f04aaa23b09cf5c3ce162fd7fc79.jpg',
+        Bn: 77,
+        En: 65,
+        Math: 45,
+        S: 42,
+        SS: 65,
+        Rel: 74,
+    },
+];
+
+
+//==================family-phonebook=============
+
+const fam = [
+
+
+
+    {
+        Name: 'Nur Islam',
+        rel: 'Father',
+        loc: 'Bogura',
+        phn: '01300536029',
+
+    },
+    {
+        Name: 'Dewara Begum',
+        rel: 'Mother',
+        loc: 'Bogura',
+        phn: '01721949231',
+
+    },
+    {
+        Name: 'Abu bakkar',
+        rel: 'Elder brother',
+        loc: 'Bogura',
+        phn: '01722135335',
+
+    },
+    {
+        Name: 'Mamun',
+        rel: 'Friends',
+        loc: 'Bogura',
+        phn: '01749254632',
+
+    },
+    {
+        Name: 'Abu sayeed',
+        rel: 'Friends',
+        loc: 'Saudi arabia',
+        phn: '01718986532',
+
+    },
+    {
+        Name: 'Abu Sabbir',
+        rel: 'Friends',
+        loc: 'Rangpur',
+        phn: '01710087532',
+
+    }
+];
---
+++ mern.js
@@ -0,0 +1,97 @@
+// const amin = document.querySelector('.box h1').style.color = "yellow";
+// const de = document.querySelector('.box p')
+
+
+// amin.innerHTML= 'Nur Amin'
+
+
+// // console.log(amin.innerHTML);
+
+
+// const anc = document.querySelector('.box , a');
+
+// anc.innerHTML= 'MORE INFO';
+
+
+
+const team = document.querySelector('.team')
+
+devs.map(data => {
+
+    console.log(document.querySelector('.team h1'));
+    team.innerHTML += `
+    
+    <div class="team-member">
+    <img src="${data.photo}">
+    
+    <h3>Id: ${data.id}</h3>
+    <h2>Name: ${data.name}</h2>
+    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, fugiat?</span>
+    <p>Location: ${data.loc}</p>
+    <p class="p1">Age: ${data.age} Years</p>
+</div>
+    
+    `
+})
+
+
+
+
+
+
+
+
+
+
+//==================student data ===============
+
+const student1 = document.querySelector('.student table tbody')
+
+stu.map((data, index) => {
+
+
+    student1.innerHTML += `
+    
+    <tr>
+     <td>${index +1}</td>
+     <td>${data.Roll}</td>
+     <td>${data.Name}</td>
+     <td>${data.Loc}</td>
+     <td>${data.Gender}</td>
+     <td>${data.Bn}</td>
+     <td>${data.En}</td>
+     <td>${data.Math}</td>
+     <td>${data.S}</td>
+     <td>${data.SS}</td>
+     <td>${data.Rel}</td>
+     <td><img src="${data.photo}"></td>
+
+     </tr>
+    
+    
+    `
+});
+
+
+
+
+
+
+
+
+//==============family-phonebook==============
+
+const ffamily = document.querySelector('.phone table tbody')
+
+fam.map((data, index) => {
+    ffamily.innerHTML += `
+  
+    <tr>
+    <td>${index+1}</td>
+    <td>${data.Name}</td>
+    <td>${data.rel}</td>
+    <td>${data.loc}</td>
+    <td>${data.phn}</td>
+    </tr>  
+    `
+})
